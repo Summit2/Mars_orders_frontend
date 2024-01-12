d47a066eb2b70fe043998c7032bd84a490e2c772
Summit2 <pinkslime987@gmail.com>
2024-01-12 22:45:03 +0300

    now works with time

--- src/components/Orders/OrdersList.tsx
+++ src/components/Orders/OrdersList.tsx
@@ -247,10 +247,16 @@ const OrdersList: FC<OrdersListProps> = ({ setPage }) => {
 export default OrdersList;
 
 function formatDate(jsonDate) {
+  if (!jsonDate) {
+    return "-";
+  }
+
   const dateObject = new Date(jsonDate);
+  const hours = dateObject.getHours().toString().padStart(2, '0');
+  const minutes = dateObject.getMinutes().toString().padStart(2, '0');
   const day = dateObject.getDate().toString().padStart(2, '0');
   const month = (dateObject.getMonth() + 1).toString().padStart(2, '0');
   const year = dateObject.getFullYear();
 
-  return `${day}.${month}.${year}`;
+  return `${parseInt(hours,10)+3}:${minutes} ${day}.${month}.${year}`;
 }
